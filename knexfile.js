31dca296e4347c7f279b515be374fb764450c667
Bryanna Keith <bryanna.dkeith@gmail.com>
2024-01-19 15:34:26 -0800

    latest commit

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://kxguvfzt:SNDH6EarGfmkvqs8D7ce7cV5FEQw8UO-@heffalump.db.elephantsql.com/kxguvfzt" // postgresql://postgres@localhost/postgres",
 } = process.env;
 
 module.exports = {
